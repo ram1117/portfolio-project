87d319d87d6b88fdc2bc1fde0b6d6397069cf9de
Yosief G. Hailay <yosiefghaylay@gmail.com>
2022-12-19 18:29:25 +0300

    added event listners to the project

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,11 @@
-let navlist = document.querySelector('.nav-list');
-// navlist.classList.replace('invisible','visible');
+let menuIcon = document.querySelector('.toggle-menu-icon');
+let navbar = document.querySelector('.nav-bar');
+let navList = document.querySelector('.nav-list');
+
+
+menuIcon.addEventListener('click',addMenuItems);
+
+function addMenuItems(){
+  navbar.classList.replace('nav-bar','nav-bar-expand')
+  navList.classList.replace('nav-list','nav-list-visible'); 
+}
