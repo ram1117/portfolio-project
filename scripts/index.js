276b4fdc9a87619a601ff590bf3e25d22e7c5761
ram1117 <ramkumark1@outlook.com>
2022-12-20 01:48:42 +0530

    add event listeners to button, menu links

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,41 @@
-let menuIcon = document.querySelector('.toggle-menu-icon');
-let navbar = document.querySelector('.nav-bar');
-let navList = document.querySelector('.nav-list');
+const menuIcon = document.querySelector('#toggle-menu-icon');
+const navbar = document.querySelector('.nav-bar');
+const navList = document.querySelector('.nav-list');
+const navListItem = document.querySelectorAll('.nav-list-item');
 
+function expandMenu() {
+  menuIcon.src = 'res-icons/ic_cross.svg';
+  menuIcon.alt = 'toggle menu cross icon';
+  navbar.classList.toggle('nav-bar-expand');
+  navList.classList.toggle('invisible');
+}
+function collapseMenu() {
+  menuIcon.src = 'res-icons/ic_menu.svg';
+  menuIcon.alt = 'toggle menu hamburger icon';
+  navbar.classList.toggle('nav-bar-expand');
+  navList.classList.toggle('invisible');
+}
 
-menuIcon.addEventListener('click',addMenuItems);
+function checkToggle() {
+  if (menuIcon.dataset.toggleMenu === 'true') {
+    expandMenu();
+    document.body.classList.toggle('scroll-lock');
+    menuIcon.dataset.toggleMenu = 'false';
+  } else {
+    collapseMenu();
+    document.body.classList.toggle('scroll-lock');
+    menuIcon.dataset.toggleMenu = 'true';
+  }
+}
 
-function addMenuItems(){
-  navbar.classList.replace('nav-bar','nav-bar-expand')
-  navList.classList.replace('nav-list','nav-list-visible'); 
+menuIcon.addEventListener('click', checkToggle);
+
+for (const listItem of navListItem) {
+  listItem.addEventListener('click', () => {
+    if (window.innerWidth <= 768) {
+      collapseMenu();
+      document.body.classList.toggle('scroll-lock');
+      menuIcon.dataset.toggleMenu = 'true';
+    }
+  });
 }
