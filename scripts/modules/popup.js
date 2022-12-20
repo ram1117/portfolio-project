ab834c32583389fb1c3afc2b358f93cc0ac6a802
Yosief G. Hailay <yosiefghaylay@gmail.com>
2022-12-20 16:02:21 +0300

    added a popup to the project

--- scripts/modules/popup.js
+++ scripts/modules/popup.js
@@ -1,3 +1,25 @@
 function createMobilePopup(proj) {
   console.log(proj.name);
+  let popup = document.createElement('div');
+  popup.classList.add('popup-container');
+  let header = document.createElement('h3');
+  header.classList.add('popup-header');
+  header.textContent = proj.name;
+  let newDiv = document.createElement('div');
+  let newPar = document.createElement('p');
+  newPar.textContent =proj.description;
+  let btn1 = document.createElement('button');
+  let btn2 = document.createElement('button');
+  
+  popup.appendChild(header);
+  popup.appendChild(newDiv);
+  popup.appendChild(newPar);
+  popup.appendChild(btn1);
+  popup.appendChild(btn2);
+
+  popup.style.height= '100vh';
+  popup.style.width = '100%';
+  let container = document.querySelector('.main-container');
+  document.body.insertBefore(popup ,container);
 }
+ 
