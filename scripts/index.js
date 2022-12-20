3cf68c9c3c28b1663c2e9d9df12db7a84b551783
ram1117 <ramkumark1@outlook.com>
2022-12-20 15:56:16 +0530

    add javascript objects for projects,add new js files

--- scripts/index.js
+++ scripts/index.js
@@ -41,3 +41,108 @@ navListItem.forEach((listItem) => {
     }
   });
 });
+
+let project1 = {
+  name: 'Project 1',
+  image: 'res-images/Rectangle 21.png',
+  imageAlt: 'Open lap top top view',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  technology: ['Ruby on Rails', 'HTML', 'CSS'],
+  buttontxt: 'See this Project',
+  livedemo: 'https://ram1117.github.io',
+  source: 'https://github.com',
+};
+let project2 = {
+  name: 'Project 2',
+  image: 'res-images/Rectangle 22.png',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  technology: ['Ruby on Rails', 'HTML', 'CSS'],
+  livedemo: '',
+  source: '',
+};
+let project3 = {
+  name: 'Project 3',
+  image: 'res-images/Rectangle 26.png',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  technology: ['Ruby on Rails', 'HTML', 'CSS'],
+  livedemo: '',
+  source: '',
+};
+let project4 = {
+  name: 'Project 4',
+  image: 'res-images/Rectangle 27.png',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  technology: ['Ruby on Rails', 'HTML', 'CSS'],
+  livedemo: '',
+  source: '',
+};
+let project5 = {
+  name: 'Project 5',
+  image: 'res-images/Rectangle 46.png',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  technology: ['Ruby on Rails', 'HTML', 'CSS'],
+  livedemo: '',
+  source: '',
+};
+let project6 = {
+  name: 'Project 6',
+  image: 'res-images/Rectangle 47.png',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  technology: ['Ruby on Rails', 'HTML', 'CSS'],
+  livedemo: '',
+  source: '',
+};
+
+function createProjectTile(projectObj) {
+  let tile = document.createElement('div');
+  tile.classList.add('project-tile');
+
+  let thumbNail = document.createElement('img');
+  thumbNail.src = projectObj.image;
+  thumbNail.alt = projectObj.imageAlt;
+  thumbNail.classList.add('project-thumbnail');
+  tile.appendChild(thumbNail);
+
+  let details = document.createElement('div');
+  details.classList.add('project-details');
+
+
+  let projTitle = document.createElement('h3');
+  projTitle.classList.add('project-title');
+  projTitle.textContent = projectObj.name;
+
+  let techContainer = document.createElement('ul');
+  techContainer.classList.add('tech-item-list');
+  
+  projectObj.technology.forEach((tech)=>
+  {
+    let techItem = document.createElement('li');
+    techItem.classList.add('tech-item');
+    techItem.textContent = tech;
+    techContainer.appendChild(techItem);
+  });
+  let projectButton = document.createElement('button');
+  projectButton.classList.add('button-generic', 'button-project');
+  projectButton.textContent = projectObj.buttontxt;
+  projectButton.onclick = function () { createPopup() };
+
+  details.appendChild(projTitle);
+  details.appendChild(techContainer);
+  details.appendChild(projectButton);
+
+  tile.appendChild(details);
+
+  return tile;
+}
+
+function createPopup() {
+  console.log('popup');
+}
+
+let projectArray = [project1, project2, project3, project4, project5, project6];
+
+let projectTile = createProjectTile(project1);
+let projectsContainer = document.querySelector('.projects-container');
+projectsContainer.appendChild(projectTile);
+
+
